daed77aa5aa6e9f313c63041a2c4e2dc9ac04281
Mattheus <mattheusp382@gmail.com>
2023-06-01 12:44:08 -0300

    commit_v35

--- scripts.js
+++ scripts.js
@@ -3,8 +3,14 @@
 const key = "d6c6d4a33ba581c47e0c4eafd3a40842"
 
 
-function cliqueiNoBotao() {
-    const input = ducoment.querySelector(".input-cidade").value
+function buscarCidade(cidade) {
 
-    console.log(input)
+    https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+    
+}
+
+function cliqueiNoBotao() {
+    const cidade = ducoment.querySelector(".input-cidade").value
+
+    buscarCidade(cidade)
 }
