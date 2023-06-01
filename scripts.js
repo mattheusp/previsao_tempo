b3717972a9ce3d2b3673cefe6a8acf95aaa0acf0
Mattheus <mattheusp382@gmail.com>
2023-06-01 12:46:22 -0300

    commit_v36

--- scripts.js
+++ scripts.js
@@ -3,8 +3,9 @@
 const key = "d6c6d4a33ba581c47e0c4eafd3a40842"
 
 
-function buscarCidade(cidade) {
+async function buscarCidade(cidade) {
 
+    const
     https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
     
 }
