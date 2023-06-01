ecae75e10164e69935f01a26e30b1d618e5d19f2
Mattheus <mattheusp382@gmail.com>
2023-06-01 14:17:39 -0300

    commit_v43

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ const key = "d6c6d4a33ba581c47e0c4eafd3a40842"
 
 function colocarDadosNaTela(dados){
     console.log(dados)
-    // document.querySelector(".cidade").innerHTML = 
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
 }
 
 async function buscarCidade(cidade) {
