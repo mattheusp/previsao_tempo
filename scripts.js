4c3e359401c7848b40834bdddd18be75d813c089
Mattheus <mattheusp382@gmail.com>
2023-06-01 14:22:33 -0300

    commit_v44

--- scripts.js
+++ scripts.js
@@ -5,10 +5,12 @@ const key = "d6c6d4a33ba581c47e0c4eafd3a40842"
 function colocarDadosNaTela(dados){
     console.log(dados)
     document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".temp").innerHTML = dados.main.temp
+    
 }
 
 async function buscarCidade(cidade) {
-    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={key}&lang=pt-br`).then( resposta => resposta.json())
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={key}&lang=pt-br&units=metric`).then( resposta => resposta.json())
    
     colocarDadosNaTela(dados)
 }
