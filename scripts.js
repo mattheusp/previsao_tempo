c2bad0ca779ff5a56524e2eceb9b1bb51e611ea2
Mattheus <mattheusp382@gmail.com>
2023-06-01 14:10:57 -0300

    commit_40

--- scripts.js
+++ scripts.js
@@ -2,18 +2,15 @@
 
 const key = "d6c6d4a33ba581c47e0c4eafd3a40842"
 
-
-async function buscarCidade(dados) {
-
-    function colorDadosNaTela(cidade){
-
-    }
-    
-    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={key}`).then( resposta => resposta.json())
-   
-    colorDadosNaTela(dados)
+function colocarDadosNaTela(dados){
+    console.log(dados)
 }
 
+async function buscarCidade(cidade) {
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={key}&lang=pt-br`).then( resposta => resposta.json())
+   
+    colocarDadosNaTela(dados)
+}
     function cliqueiNoBotao() {
     const cidade = ducoment.querySelector(".input-cidade").value
 
